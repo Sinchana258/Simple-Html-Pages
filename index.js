e0ab8b4ae52f172f1615da958a42d996807db732
Sinchana T <sinchanat826@gmail.com>
2025-08-29 21:25:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
-
 import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from "path";
